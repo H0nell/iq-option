5bbf0d515c0eacb3e3d16374eb5cc33b99ffaa3b
root <root@epd2alrf3mmnc004togg.auto.internal>
2019-08-19 10:57:49 +0000

    100%

--- script.py
+++ script.py
@@ -4,12 +4,12 @@ from minio import Minio
 from minio.error import ResponseError
 
 minioClient = Minio('192.168.1.18',
-               access_key='ASDsdadsafasfasfRFSAFS',
-               secret_key='sdadsasdasfFGASFASFVvac',
+               access_key='123',
+               secret_key='12345678',
                secure=False)
 try:
     data = minioClient.get_partial_object('bucket', 'test (1).jpg', 1000)
-    with open('my-testfile', 'wb') as file_data:
+    with open('iq-option.jpg', 'wb') as file_data:
         for d in data:
             file_data.write(d)
 except ResponseError as err:
